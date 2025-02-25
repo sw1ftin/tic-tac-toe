8719deedc3942a1f7e46f5e67f05ac379d5fe8fd
Jakob Ornysh <jornysh@gmail.com>
2025-02-25 17:15:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let field;
+let isEnded;
+let zeroTurn;
+let winIndicies;
+
 startGame();
 addResetListener();
 
 function startGame () {
     renderGrid(3);
+    field = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY];
+    isEnded = false;
+    zeroTurn = true;
 }
 
 function renderGrid (dimension) {
@@ -26,10 +34,49 @@ function renderGrid (dimension) {
     }
 }
 
+function check(field, a, b, c) {
+    if (field[a] === field[b] && field[b] === field[c] && field[c] !== EMPTY) {
+        winIndicies = [a, b, c];
+        return true;
+    }
+    return false;
+}
+
+function checkWin () {
+    return (check(field, 0, 1, 2) || check(field, 3, 4, 5) || check(field, 6, 7, 8) ||
+        check(field, 0, 3, 6) || check(field, 1, 4, 7) || check(field, 2, 5, 8) ||
+        check(field, 0, 4, 8) || check(field, 2, 4, 6));
+}
+
 function cellClickHandler (row, col) {
     // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
 
+    if (isEnded === true || field[row * 3 + col] !== EMPTY)
+        return;
+
+    if (zeroTurn === true) {
+        field[row * 3 + col] = ZERO;
+        renderSymbolInCell(ZERO, row, col);
+    } else {
+        field[row * 3 + col] = CROSS;
+        renderSymbolInCell(CROSS, row, col);
+    }
+
+    isEnded = checkWin();
+
+    if (isEnded === true) {
+        for (let i in winIndicies)
+            renderSymbolInCell(zeroTurn ? ZERO : CROSS, Math.floor(winIndicies[i] / 3) , winIndicies[i] % 3, 'red');
+        alert(`${zeroTurn? 1 : 2}-ый игрок победил!`);
+        isEnded = true;
+    }
+    else if (field.indexOf(EMPTY) === -1) {
+        alert('Победила дружба');
+        isEnded = true;
+    }
+
+    zeroTurn = !zeroTurn;
 
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
@@ -55,6 +102,7 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame();
 }
 
 
